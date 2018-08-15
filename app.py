a62ade7aca40e8494c047de86700cb0f22ee7de8
aboud20-meet <aboud20@meet.mit.edu>
2018-08-15 21:58:32 +0300

    oi oi oi

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 
 from flask import Flask, flash, render_template, url_for, redirect, request, session as flask_session
-#from flask.ext.session import Session
 from databases import *
 
 
-# Starting the flask app
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-#Session(app)
 
-# App routing code here
+
 @app.route('/')
 def home():
     print(flask_session)
     posts =query_all_posts() 
+    posts = posts[::-1]
     if 'username' in flask_session:
         return render_template('home_loggedin.html',name="logged in as : " +flask_session['username'],posts=posts)
     else :
         return render_template('home.html',name=" ",posts=posts)
-
-#@app.route('/<int:id_table>')
-#def home_loggedin(id_table):
- #   user1 = query_user_by_id(id_table)
-  #  posts = query_all_posts()
-   # return render_template('home_loggedin.html',name =user1.name, posts= posts)
 
 @app.route('/create-post',methods=['GET','POST'])
 def create_post(): 
@@ -53,11 +45,15 @@ def log_in():
         name = request.form['uname']
         password = request.form['psw']
         posts = query_all_posts()
+        posts = posts.reverse()
+
         user = query_by_name_and_password(name, password)
         if user is not None and user.password == password:
             flask_session['username'] = user.name
             return redirect(url_for('home'))
         else :
+            error = 'Username & Password do not match , Please try again'
+            flash(error)
             return render_template('log_in.html')
     else:       
         return render_template('log_in.html')
@@ -75,8 +71,6 @@ def sign_up():
         return render_template('sign_up.html')
 
 
-
-# Running the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
 
